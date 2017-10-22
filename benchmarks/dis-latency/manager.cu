788200e3e3ed25d065f3006103262ab14cbf2083
Jonas Markussen <jonassm@ifi.uio.no>
2017-10-22 15:48:38 +0200

    Work in progress

--- benchmarks/dis-latency/manager.cu
+++ benchmarks/dis-latency/manager.cu
@@ -2,8 +2,15 @@
 #include <iostream>
 #include <iomanip>
 #include <stdexcept>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
 #include <cstring>
 #include <cerrno>
+#include <ctime>
+#include <cstdlib>
+#include <csignal>
+#include <sys/time.h>
 #include <nvm_types.h>
 #include <nvm_ctrl.h>
 #include <nvm_manager.h>
@@ -15,48 +22,80 @@ using std::cerr;
 using std::endl;
 
 
-Settings::Settings()
+static std::mutex mtx;
+static std::condition_variable cv;
+static bool stopRunning = false;
+
+
+static uint16_t randomId()
 {
-    cudaDevice = -1;
-    //controllerId = 0;
-    controllerId = 0x10000;
-    disAdapter = 0;
-    nvmNamespace = 1;
+    static uint16_t uniqueCounter = 0;
+
+    timeval tv;
+    srand(time(NULL));
+
+    if (gettimeofday(&tv, NULL) == 0)
+    {
+        srand(tv.tv_usec);
+    }
+
+    return (rand() + uniqueCounter++) & 0xffff;
 }
 
 
-static void runManager(nvm_ctrl_t controller, const Settings& settings)
+static void stopManager()
 {
-    SegmentPtr aqSegment(createSegment(randomId(), 2 * controller->page_size));
+    std::lock_guard<std::mutex> lock(mtx);
+    stopRunning = true;
+    cv.notify_all();
+}
 
-//
-//    SegmentPtr adminQueueSegment;
-//    DmaPtr adminQueues;
-//        adminQueueSegment = createSegment(0, 2 * controller->page_size);
-//
-//        adminQueues = createDmaMapping(adminQueueSegment, controller, settings.disAdapter);
 
+static void runManager(nvm_ctrl_t controller, const ManagerSettings& settings)
+{
+    SegmentPtr targetBuffer(createSegment(randomId(), controller->page_size, settings.cudaDevice));
+
+    DmaPtr targetWindow(createDmaMapping(targetBuffer, controller, settings.ctrlAdapter));
+
+    for (uint32_t adapter: settings.adapters)
+    {
+        exportSegment(targetBuffer, adapter);
+    }
+
+    cerr << std::hex << (*targetWindow)->ioaddrs[0] << endl;
+    
+    std::unique_lock<std::mutex> lock(mtx);
+    auto old = signal(SIGINT, (void (*)(int)) stopManager);
+
+    cv.wait(lock, [] { return stopRunning; });
+
+    signal(SIGINT, old);
+    lock.unlock();
 }
 
 
 int main()
 {
-    Settings settings;
+    ManagerSettings settings;
 
     nvm_ctrl_t controller;
     sci_error_t err;
 
     SCIInitialize(0, &err);
 
-    int nvmerr = nvm_dis_ctrl_init(&controller, settings.controllerId, settings.disAdapter);
+    int nvmerr = nvm_dis_ctrl_init(&controller, settings.controllerId, settings.ctrlAdapter);
     if (nvmerr != 0)
     {
         cerr << "Failed to get controller reference: " << std::strerror(nvmerr) << endl;
         return 1;
     }
 
+    SegmentPtr queueMemory;
+    DmaPtr mapping;
     try
     {
+        queueMemory = createSegment(randomId(), 2 * controller->page_size);
+        mapping = createDmaMapping(queueMemory, controller, settings.ctrlAdapter);
     }
     catch (const std::runtime_error& err)
     {
@@ -65,6 +104,29 @@ int main()
         return 1;
     }
 
+    nvm_manager_t manager = nullptr;
+    nvmerr = nvm_manager_register(&manager, controller, *mapping.get());
+    if (nvmerr != 0)
+    {
+        nvm_ctrl_free(controller);
+        cerr << "Failed to reset controller" << endl;
+        return 1;
+    }
+
+    try
+    {
+        runManager(controller, settings);
+    }
+    catch (const std::runtime_error& err)
+    {
+        nvm_manager_unregister(manager);
+        nvm_ctrl_free(controller);
+
+        cerr << err.what() << endl;
+        return 1;
+    }
+
+    nvm_manager_unregister(manager);
     nvm_ctrl_free(controller);
     SCITerminate();
     return 0;
--- benchmarks/dis-latency/settings.h
+++ benchmarks/dis-latency/settings.h
@@ -1,18 +1,40 @@
 #ifndef __DIS_LATENCY_SETTINGS_H__
 #define __DIS_LATENCY_SETTINGS_H__
 
+#include <vector>
 #include <cstddef>
 #include <cstdint>
 
 
-struct Settings
+struct ManagerSettings
 {
     int         cudaDevice;
     uint64_t    controllerId;
-    uint32_t    disAdapter;
+    uint32_t    ctrlAdapter;
+    std::vector<uint32_t> adapters;
+
+    ManagerSettings()
+    {
+        cudaDevice = -1;
+        //controllerId = 0;
+        controllerId = 0x10000;
+        ctrlAdapter = 0;
+    }
+};
+
+
+struct ClientSettings
+{
+    uint64_t    controllerId;
+    uint32_t    ctrlAdapter;
     uint32_t    nvmNamespace;
 
-    Settings();
+    ClientSettings()
+    {
+        controllerId = 0x10000;
+        ctrlAdapter = 0;
+        nvmNamespace = 1;
+    }
 };
 
 #endif
