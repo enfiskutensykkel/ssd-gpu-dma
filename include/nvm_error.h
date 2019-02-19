92aa4522bffe1abc4e77a0a00d69a93e6d8e1e78
Patrick Stewart <patrick@rfcreations.com>
2019-02-19 20:22:10 +0000

    Correct status code extraction in nvm_error

--- include/nvm_error.h
+++ include/nvm_error.h
@@ -12,17 +12,17 @@ extern "C" {
 
 
 /* Get the status code type of an NVM completion. */
-#define NVM_ERR_SCT(cpl)            ((uint8_t) _RB(*NVM_CPL_STATUS(cpl), 27, 25))
+#define NVM_ERR_SCT(cpl)            ((uint8_t) _RB(*NVM_CPL_STATUS(cpl), 11, 9))
 
 
 
 /* Get the status code of an NVM completion */
-#define NVM_ERR_SC(cpl)             ((uint8_t) _RB(*NVM_CPL_STATUS(cpl), 24, 17))
+#define NVM_ERR_SC(cpl)             ((uint8_t) _RB(*NVM_CPL_STATUS(cpl), 8, 1))
 
 
 
 /* Is do not retry flag set? */
-#define NVM_ERR_DNR(cpl)            ((uint8_t) _RB(*NVM_CPL_STATUS(cpl), 31, 31))
+#define NVM_ERR_DNR(cpl)            ((uint8_t) _RB(*NVM_CPL_STATUS(cpl), 15, 15))
 
 
 
@@ -45,8 +45,8 @@ extern "C" {
 
 /* Extract values from packed status */
 #define NVM_ERR_UNPACK_ERRNO(status)    ((status > 0) ? (status) : 0)
-#define NVM_ERR_UNPACK_SCT(status)      ((status < 0) ? (((status) >> 8) & 0xff) : 0)
-#define NVM_ERR_UNPACK_SC(status)       ((status < 0) ? ((status) & 0xff) : 0)
+#define NVM_ERR_UNPACK_SCT(status)      ((status < 0) ? (((-status) >> 8) & 0xff) : 0)
+#define NVM_ERR_UNPACK_SC(status)       ((status < 0) ? ((-status) & 0xff) : 0)
 
 
 /* Check if everything is okay */
